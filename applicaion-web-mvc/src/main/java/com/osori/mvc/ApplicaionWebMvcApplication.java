4839c43559e342829a1588ba181ebed6a9640f63
OsoriAndOmori <skywhite15@korea.ac.kr>
2022-07-22 15:19:11 +0900

    gradle 정리

---
+++ applicaion-web-mvc/src/main/java/com/osori/mvc/ApplicaionWebMvcApplication.java
@@ -0,0 +1,15 @@
+package com.osori.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@SpringBootApplication
+public class ApplicaionWebMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicaionWebMvcApplication.class, args);
+    }
+
+}
---
+++ applicaion-web-mvc/src/test/java/com/osori/mvc/ApplicaionWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osori.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicaionWebMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
