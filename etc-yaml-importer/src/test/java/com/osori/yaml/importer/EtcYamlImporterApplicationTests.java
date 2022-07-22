94d19765f6065513cd730a11e44e767055564351
OsoriAndOmori <skywhite15@korea.ac.kr>
2022-07-22 15:05:18 +0900

    yaml importer setting + polishing

---
+++ etc-yaml-importer/src/test/java/com/osori/yaml/importer/EtcYamlImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osori.yaml.importer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtcYamlImporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
